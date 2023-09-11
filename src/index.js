37bd191ad8e83d3f5025eac9eb11520e83c04a1a
carolinacartaxo <carolinamcartaxo@gmail.com>
2023-09-10 23:35:59 -0300

    On JS I created a function to return me the formated date and  hours

---
+++ src/index.js
@@ -0,0 +1,27 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay(); // It will return the day position index - 0 to 6
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+let currentTime = new Date();
