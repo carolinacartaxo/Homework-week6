b084bcce7d938066737ea7cd27cec7393b12cb09
carolinacartaxo <carolinamcartaxo@gmail.com>
2023-09-10 23:40:28 -0300

    Created the event to select my Current Location button

--- src/index.js
+++ src/index.js
@@ -57,3 +57,6 @@ dateElement.innerHTML = formatDate(currentTime);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
